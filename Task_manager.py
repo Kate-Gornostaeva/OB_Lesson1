55a80cccf261740ff27cd630c7f0e6a0e2b6de6e
Kate-Gornostaeva <itsmekate153@gmail.com>
2024-11-16 18:44:57 +1000

    task_manager_ready
    готовый результат task_manager для задания ОВ01

--- Task_manager.py
+++ Task_manager.py
@@ -4,24 +4,52 @@
 # задач и вывода списка текущих (не выполненных) задач.
 
 class Task():
+    task_list = []
     def __init__(self, description, deadline):
         self.description = description
         self.deadline = deadline
         self.status = False
 
-task_list = []
 
     def add_task(self):
-        print("Введите задачу: ")
-        self.task_list.append ()
+        description = input ("Введите задачу: ")
+        deadline = input("Срок выполнения задачи:  ")
+        task = Task(description, deadline)
+        Task.task_list.append (task)
 
 
-    def mark_task (self):
-        self.status = True
+    def mark_task (self, index):
+        if index >=0 and index <= len(Task.task_list):
+            Task.task_list[index].status = True
+        else:
+            print("Неверный индекс задачи")
 
     def current_tasks (self):
-        for c, value in enumerate(self.task_list, 1):
-            print(c, value)
+        for index, task in enumerate(Task.task_list):
+            if not task.status:
+                print(f"{index+1}. {task.description}. Выполнить {task.deadline}")
+
+task_manager = Task('', '')
+while True:
+    print("\nВаш список задач:")
+    task_manager.current_tasks()
+    print("\nВыберите действие:")
+    print("1 - добавить задачу")
+    print("2 - отметить задачу как выполненную")
+    print("3 - завершить работу со списком задач")
+    user_choice = int(input(":"))
+
+    if user_choice == 1:
+        task_manager.add_task()
+    elif user_choice == 2:
+        task_index = int(input("Введите индекс выполненной задачи:  ")) - 1
+        task_manager.mark_task(task_index)
+    elif user_choice == 3:
+        break
+    else:
+        print("Неверный выбор. Введите номер действия 1, 2 или 3")
+
+
 
 
 
